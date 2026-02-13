0abd12efbb1fcadfbd1b8094abe6e4d4b88af3fc
deweesa <asadeweese@gmail.com>
2026-02-12 20:47:58 -0800

    Modifying program file

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -1,7 +1,28 @@
-﻿var counter = 0;
-var max = args.Length is not 0 ? Convert.ToInt32(args[0]) : -1;
+﻿internal class Program
+{
+    private static async Task<int> Main(string[] args)
+    {
+        if(args.Length == 0)
+        {
+            Console.WriteLine("Please enter url");
+            return -1;
+        }
+        string url = args[0];
 
-while(max is -1 || counter < max) {
-    Console.WriteLine($"Counter: {++counter}");
-    await Task.Delay(TimeSpan.FromMilliseconds(1_000));
+        Console.WriteLine($"Url to scrape: {url}");
+
+        return 1;
+    }
+
+    private static async Task OldMain(string[] args)
+    {
+        var counter = 0;
+        var max = args.Length is not 0 ? Convert.ToInt32(args[0]) : -1;
+
+        while (max is -1 || counter < max)
+        {
+            Console.WriteLine($"Counter: {++counter}");
+            await Task.Delay(TimeSpan.FromMilliseconds(1_000));
+        }
+    }
 }
