896d760ba62eb3498270d1a4c7cbb193c6517f4c
deweesa <asadeweese@gmail.com>
2026-02-15 15:17:54 -0800

    Reorganizing project structure

---
+++ src/Program/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.CommandLine;
+
+class Program
+{
+    private static int Main(string[] args)
+    {
+        Option<int> depthOption = new("depth", ["-d", "--depth"])
+        {
+            Description = "Maximum depth for crawler to search",
+            DefaultValueFactory = parseResult => 5
+        };
+
+        Option<int> breadthOption = new("breadth", ["-b", "--breadth"])
+        {
+            Description = "Maximum breadth for crawler to search",
+            DefaultValueFactory = parseResult => 5
+        };
+
+        Option<string> pageOption = new("page", ["-p", "--page"])
+        {
+            Description = "Starting page"
+        };
+
+        RootCommand rootCommand = new("Console App for running WebCrawler");
+
+        rootCommand.Options.Add(depthOption);
+        rootCommand.Options.Add(breadthOption);
+        rootCommand.Options.Add(pageOption);
+
+        rootCommand.SetAction(parseResult =>
+        {
+            int depth = parseResult.GetValue(depthOption);
+            int breadth = parseResult.GetValue(breadthOption);
+            string? page = parseResult.GetValue(pageOption);
+        });
+
+        ParseResult parseResult = rootCommand.Parse(args);
+        return parseResult.Invoke();
+    }
+}
