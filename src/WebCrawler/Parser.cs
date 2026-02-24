935a21b50e0e33f5c61ef18f6041f8c3a63c4508
deweesa <asadeweese@gmail.com>
2026-02-23 16:22:44 -0800

    #1 adding failing unit tests

--- src/WebCrawler/Parser.cs
+++ src/WebCrawler/Parser.cs
@@ -2,20 +2,28 @@ using HtmlAgilityPack;
 
 namespace WebCrawler;
 
+/*
+<summary>
+Class responsible for parsing html from the wiki pages. Life cycle will be intiated
+by some sort of orchestrator class that will make the calls to the web.
+</summary>
+*/
 public class Parser
 {
     private string pageHtml;
-    private string pageTitle;
+    public string pageTitle;
     private HtmlDocument pageDocument;
     public HashSet<string> linkedPages;
+    private int maxOutLinks;
 
-    public Parser(string _pageTitle, string _pageHtml)
+    public Parser(string _pageTitle, string _pageHtml, int _maxOutLinks = -1)
     {
         pageTitle = _pageTitle;
         pageHtml = _pageHtml;
         pageDocument = new HtmlDocument();
         pageDocument.LoadHtml(pageHtml);
         linkedPages = new HashSet<string>();
+        maxOutLinks = _maxOutLinks;
     }
 
     public Parser(string _pageHtml)
@@ -25,6 +33,13 @@ public class Parser
         pageDocument = new HtmlDocument();
         pageDocument.LoadHtml(pageHtml);
         linkedPages = new HashSet<string>();
+        maxOutLinks = -1;
+    }
+
+    public void Parse()
+    {
+        ExtractTitle();
+        ExtractLinks();
     }
 
     public void ExtractLinks()
--- test/WebCrawler.Tests/ParserTests.cs
+++ test/WebCrawler.Tests/ParserTests.cs
@@ -1,10 +1,34 @@
+
 namespace WebCrawler.Tests;
 
 public class UnitTest1
 {
+    private string filePath = "./assets/AbrahamLincoln.html";
+    private string title = "Abraham Lincoln";
+    private string fileCouldNotBeRead = "File could not be read:";
     [Fact]
     public void Test1()
     {
+        Assert.True(true);
+    }
 
+    [Fact]
+    public void ShouldGetTitleFromPage()
+    {
+        string html = null;
+        try
+        {
+            using StreamReader streamReader = new(filePath);
+            html = streamReader.ReadToEnd();
+        }
+        catch(IOException e)
+        {
+            Console.WriteLine(fileCouldNotBeRead);
+            Console.WriteLine(e.Message);
+            Assert.Fail(e.Message);
+        }
+
+        Parser parser = new Parser(html);
+        Assert.Equal(title, parser.ExtractTitle());
     }
 }
