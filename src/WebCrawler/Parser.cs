92d288c27102fc9bfb17dff55c66f712d0d948ba
deweesa <asadeweese@gmail.com>
2026-02-26 19:54:33 -0800

    #1 Initial parsing of wiki pages completed

--- src/WebCrawler/Parser.cs
+++ src/WebCrawler/Parser.cs
@@ -8,17 +8,20 @@ Class responsible for parsing html from the wiki pages. Life cycle will be intia
 by some sort of orchestrator class that will make the calls to the web.
 </summary>
 */
+//todo: what does a link node look like?
+// - obv they're a tags
+// - they all have /wiki/ in the href
 public class Parser
 {
     private string pageHtml;
     public string pageTitle;
+    private int prefixLength = "/wiki/".Length; 
     private HtmlDocument pageDocument;
     public HashSet<string> linkedPages;
     private int maxOutLinks;
 
-    public Parser(string _pageTitle, string _pageHtml, int _maxOutLinks = -1)
+    public Parser(string _pageHtml, int _maxOutLinks)
     {
-        pageTitle = _pageTitle;
         pageHtml = _pageHtml;
         pageDocument = new HtmlDocument();
         pageDocument.LoadHtml(pageHtml);
@@ -29,7 +32,6 @@ public class Parser
     public Parser(string _pageHtml)
     {
         pageHtml = _pageHtml;
-        pageTitle = ExtractTitle();
         pageDocument = new HtmlDocument();
         pageDocument.LoadHtml(pageHtml);
         linkedPages = new HashSet<string>();
@@ -38,17 +40,31 @@ public class Parser
 
     public void Parse()
     {
-        ExtractTitle();
-        ExtractLinks();
+        ParseTitle();
+        ParseLinks();
     }
 
-    public void ExtractLinks()
+    public void ParseLinks()
     {
-        throw new NotImplementedException();
+        pageDocument.DocumentNode.SelectNodes("//a[@href]")?.ToList().ForEach(node =>
+        {
+            string hrefValue = node.GetAttributeValue("href", string.Empty);
+            if (hrefValue.StartsWith("/wiki/") && !hrefValue.Contains(":"))
+            {
+                hrefValue = hrefValue[prefixLength..]; //todo: this is a bit hacky, maybe we should be doing something more robust here? also this only works for well formed links
+                linkedPages.Add(hrefValue);
+            }
+        });
+
+        if (maxOutLinks != -1 && linkedPages.Count > maxOutLinks)
+        {
+            linkedPages = linkedPages.Take(maxOutLinks).ToHashSet(); //todo: this is also a bit hacky, maybe we should be doing something more robust here?
+        }
     } 
 
-    public string ExtractTitle()
+    public void ParseTitle()
     {
-        throw new NotImplementedException();
+        HtmlNode titleElement  = pageDocument.DocumentNode.SelectSingleNode("//span[@class='mw-page-title-main']");
+        pageTitle = titleElement?.InnerText; //todo: handle null case, this only works for well formed title nodes
     }
 }
--- test/WebCrawler.Tests/ParserTests.cs
+++ test/WebCrawler.Tests/ParserTests.cs
@@ -1,24 +1,99 @@
 
 namespace WebCrawler.Tests;
 
-public class UnitTest1
+public class ParserTests
 {
-    private string filePath = "./assets/AbrahamLincoln.html";
+    private string filePath = Path.Combine(AppContext.BaseDirectory, "assets", "AbrahamLincoln.html");
     private string title = "Abraham Lincoln";
     private string fileCouldNotBeRead = "File could not be read:";
+
+    // todo: testing
+    // - [ ] testing for bad pages
+    // - [ ] testing for pages with no links
+
+    //Unit tests noted with Real in the title are tests ran against html files pulled from the wiki, more as integration testing
     [Fact]
-    public void Test1()
+    public void ShouldGetTitleFromRealPage()
     {
-        Assert.True(true);
+        //Arrange
+        string html = GetHtmlFromFile(filePath);
+        Parser parser = new Parser(html);
+
+        //Act
+        parser.ParseTitle();
+
+        //Assert
+        Assert.Equal(title, parser.pageTitle);
     }
 
     [Fact]
-    public void ShouldGetTitleFromPage()
+    public void ShouldGetAnyLinksFromRealPage()
     {
-        string html = null;
+        //Arrange
+        string html = GetHtmlFromFile(filePath);
+        Parser parser = new Parser(html);
+
+        //Act
+        parser.ParseLinks();
+
+        //Assert
+        Assert.NotEmpty(parser.linkedPages);        
+    }
+
+    [Fact]
+    public void ShouldGetCorrectLinksFromMockedPage()
+    {
+        //Arrange
+        string html = @"<html><body><a href=""/wiki/Link1"">Link1</a><a href=""/wiki/Link2"">Link2</a><a href=""/wiki/Link3"">Link3</a></body></html>";
+        Parser parser = new Parser(html);
+
+        //Act
+        parser.ParseLinks();
+
+        //Assert
+        Assert.Equal(3, parser.linkedPages.Count);
+        Assert.Contains("Link1", parser.linkedPages);
+        Assert.Contains("Link2", parser.linkedPages);
+        Assert.Contains("Link3", parser.linkedPages);
+    }
+
+    [Fact]
+    public void ShouldNotGetLinksWithColonsFromMockedPage()
+    {
+        //Arrange
+        string html = @"<html><body><a href=""/wiki/Link1"">Link1</a><a href=""/wiki/Link:2"">Link2</a><a href=""/wiki/Link3"">Link3</a></body></html>";
+        Parser parser = new Parser(html);
+
+        //Act
+        parser.ParseLinks();
+
+        //Assert
+        Assert.Equal(2, parser.linkedPages.Count);
+        Assert.Contains("Link1", parser.linkedPages);
+        Assert.Contains("Link3", parser.linkedPages);
+    }
+    
+    [Fact]
+    public void ShouldNotGetMoreThanMaxOutLinksFromMockedPage()
+    {
+        //Arrange
+        string html = @"<html><body><a href=""/wiki/Link1"">Link1</a><a href=""/wiki/Link2"">Link2</a><a href=""/wiki/Link3"">Link3</a></body></html>";
+        Parser parser = new Parser(html, 2);
+
+        //Act
+        parser.ParseLinks();
+
+        //Assert
+        Assert.Equal(2, parser.linkedPages.Count);
+    }
+
+    #region Helper Methods
+    private string GetHtmlFromFile(string path)
+    {
+        string html = string.Empty;
         try
         {
-            using StreamReader streamReader = new(filePath);
+            using StreamReader streamReader = new(path);
             html = streamReader.ReadToEnd();
         }
         catch(IOException e)
@@ -28,8 +103,7 @@ public class UnitTest1
             Assert.Fail(e.Message);
         }
 
-        Assert.True(true);
-        // Parser parser = new Parser(html);
-        // Assert.Equal(title, parser.ExtractTitle());
+        return html;
     }
+    #endregion
 }
